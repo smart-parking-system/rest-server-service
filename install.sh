19a0b0bb512c2de85e2dd3d7283c5b1f2ed185b8
Maksym Tkachuk <s2m4f6@gmail.com>
2021-12-08 20:10:37 +0200

    Deleted usage

--- install.sh
+++ install.sh
@@ -21,36 +21,17 @@ install() {
   cd /sps
   wget https://github.com/smart-parking-system/rest-server/releases/download/$VERSION/sps-rest-server.jar
 
-  echo -e "SPS Rest Server is Installed\nTo run: /etc/init.d/sps-rest-server start"
+  echo -e "SPS Rest Server is installed succesfully\nTo run: /etc/init.d/sps-rest-server start"
 }
 
 uninstall() {
-  rm -rf /etc/rc.d/S99sps-rest-server /etc/rc3.d/S99/sps-rest-server /etc/init.d/sps-rest-server /sps
+  rm -rf /etc/rc.d/S99sps-rest-server /etc/rc3.d/S99sps-rest-server /etc/init.d/sps-rest-server /sps
+  echo "SPS Rest Server is uninstalled succesfully"
 }
 
-usage() {
-  echo "SPS Rest Server Service Installaation script"
-  echo "Running with no arguments runs install"
-  echo "Usage: $0 [help|install|uninstall]"
-  exit 0
-}
+if [[ "$1" == "uninstall" ]]; then
+  uninstall
+else
+  install
+fi
 
-main() {
-  case "$1" in
-      install)
-         install
-         ;;
-      unistall)
-         unistall
-         ;;
-      help)
-         usage
-         ;;
-      *)
-        install
-  esac
-
-  exit 0
-}
-
-main $@
